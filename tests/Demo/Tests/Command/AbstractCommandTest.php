280e976de709fb4854c9baec0c2d19ff32e5ee7e
Toni Uebernickel <tuebernickel@gmail.com>
2012-01-05 14:47:44 +0100

    add test on AbstractCommand mocking execute method

--- tests/Demo/Tests/Command/AbstractCommandTest.php
+++ tests/Demo/Tests/Command/AbstractCommandTest.php
@@ -20,4 +20,29 @@ class AbstractCommandTest extends \PHPUnit_Framework_TestCase
         $commandTester = new CommandTester($command);
         $commandTester->execute(array('--append' => true));
     }
+
+    public function testExecuteWithFile()
+    {
+        $command = $this->getMockForAbstractClass(
+            'Demo\Command\AbstractCommand',
+            array('TestRunOk'),
+            'TestRunOkCommand',
+            true,
+            true,
+            true,
+            array('execute')
+        );
+
+        $command
+            ->expects($this->once())
+            ->method('execute')
+            ->will($this->returnValue(0))
+        ;
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array());
+
+        // Reaching this point means no error occured in initialize().
+        $this->assertTrue(true);
+    }
 }
